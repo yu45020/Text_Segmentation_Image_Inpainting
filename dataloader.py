7aaeb5d35ee949ca6d3c2ed0f3644c9bad929f61
yu45020 <yu45020@gmail.com>
2018-06-25 14:25:32 -0700

    put all image augment into dataset rather than dataloader
    Former-commit-id: b5e975e4d90108f23a80c35b06a304c97aa8d582
    Former-commit-id: a3df573c661e5065c00b4aac409f2af31ecd5e21

--- dataloader.py
+++ dataloader.py
@@ -3,14 +3,12 @@ import os
 import random
 import re
 from itertools import chain
-from multiprocessing import cpu_count
-from multiprocessing.dummy import Pool as ThreadPool
 
 import torch
 from PIL import Image
 from torch import nn
 from torch.nn.functional import pad
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 from torchvision.transforms import ColorJitter, ToTensor, RandomResizedCrop, Compose, Normalize, transforms
 from torchvision.transforms.functional import resized_crop, to_tensor
 
@@ -22,104 +20,49 @@ Tensor = FloatTensor
 Filter difference in brightness, to some degree. 
 If this value is set very high, say 0.8, some words are hard to detect;
 if too small, say <0.1, the mask may have noisy white points, and the model will fail to converge. 
-Recommend to generate masks to check the optimal value
+VERY IMPORTANT: Generate masks before dumping data into the model. Noisy data or almost black masks hurt performances.
 """
-brightness_filter = 0.3  # in [0,1]
+brightness_difference = 0.3  # in [0,1]
 
 
-class ImageSet(Dataset):
-    def __init__(self, img_folder, max_img=False):
+class TextSegmentationData(Dataset):
+    def __init__(self, img_folder, max_img=False, img_size=(512, 512)):
         # get raw images
-        self.images = glob.glob(os.path.join(img_folder, "raw/*"), recursive=True)
+        self.images = glob.glob(os.path.join(img_folder, "raw/*"))
         assert len(self.images) > 0
         self.max_img = max_img
+        # if len(self.images) > max_img and max_img:
+        #     self.images = random.choices(self.images, k=max_img)
+        self.images = random.choices(self.images, k=max_img)
         print("Find {} images. ".format(len(self.images)))
 
-    def __len__(self):
-        return self.max_img
-
-    def __getitem__(self, item):
-        img_file = random.choice(self.images)
-        # avoid multiprocessing on the same image
-        img_raw_ = Image.open(img_file).convert('RGB')
-        img_raw = Image.new('RGB', img_raw_.size)
-        img_core = img_raw_.getdata()
-        img_raw.putdata(img_core)
-
-        img_clean_ = Image.open(re.sub("raw", 'clean', img_file)).convert("RGB")
-        img_clean = Image.new("RGB", img_clean_.size)
-        img_core_ = img_clean_.getdata()
-        img_clean.putdata(img_core_)
-        return img_raw, img_clean
-
-
-class MaskLoader(DataLoader):
-    # mask: 1 is masked word; 0 is background
-    def __init__(self, dataset,
-                 batch_size=2,
-                 shuffle=True,
-                 img_size=(512, 512),
-                 num_workers=0
-                 ):
-        super(MaskLoader, self).__init__(dataset,
-                                         batch_size,
-                                         shuffle,
-                                         num_workers=num_workers,
-                                         collate_fn=self.batch_collector)
-
-        assert all(x % 16 == 0 for x in img_size), 'Image size must be multiple of 16'
         self.img_size = img_size
-        self.dataset = dataset
-        self.color_jitter = ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2)
-
-        # MobileNet V2 pre-train checkpoint needs to normalize image
-        # https://github.com/tonylins/pytorch-mobilenet-v2/issues/9
-        self.transformer = Compose([ToTensor(),
+        # image augment
+        self.transformer = Compose([ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+                                    ToTensor(),
                                     Normalize(mean=[0.485, 0.456, 0.406],
                                               std=[0.229, 0.224, 0.225])])
 
-    def batch_collector(self, batch):
-        raw_clean = self.image_augment(batch)
+    def __len__(self):
+        return len(self.images)
 
-        raw_imgs = [i[0] for i in raw_clean]
-        raw_imgs = torch.stack(raw_imgs, dim=0).contiguous()
+    def __getitem__(self, item):
+        img_file = self.images[item]
+        # avoid multiprocessing on the same image
+        img_raw = Image.open(img_file).convert('RGB')
+        img_clean = Image.open(re.sub("raw", 'clean', img_file)).convert("RGB")
+        img_raw, img_mask = self.process_images(img_raw, img_clean)
+        return img_raw, img_mask
 
-        mask_imgs = [i[1] for i in raw_clean]
-        # masks are in single channel
-        mask_imgs = torch.stack(mask_imgs, dim=0).contiguous()
-
-        # clean_img_masks = torch.abs(clean_imgs - raw_imgs)  # usually use white to cover words
-        # clean_img_masks = clean_img_masks[:, 0, :, :]
-        # clean_img_masks = torch.where(clean_img_masks > brightness_filter,  # filter difference in brightness
-        #                               torch.ones_like(clean_img_masks),
-        #                               torch.zeros_like(clean_img_masks))
-        # clean_img_masks = clean_img_masks.long()
-        # if use_cuda:
-        #     raw_imgs = raw_imgs.cuda()
-        #     mask_imgs = mask_imgs.cuda(async=True)
-
-        return raw_imgs, mask_imgs
-
-    def image_augment(self, pil_imgs):
-        with ThreadPool(cpu_count()) as p:
-            out = p.map(self._image_augment, pil_imgs)
-        return out
-
-    def _image_augment(self, pil_img):
-        raw, clean = pil_img
-        # crop then resize
+    def process_images(self, raw, clean):
         i, j, h, w = RandomResizedCrop.get_params(raw, scale=(0.1, 2.0), ratio=(3. / 4., 4. / 3.))
         raw_img = resized_crop(raw, i, j, h, w, size=self.img_size)
         clean_img = resized_crop(clean, i, j, h, w, self.img_size)
 
         # get mask before further image augment
-        mask_tensor = self.get_mask(raw_img, clean_img)
-
-        # add color noise
-        raw_img = self.color_jitter(raw_img)
-        # to tensor
+        mask_tensor_long = self.get_mask(raw_img, clean_img)
         raw_img = self.transformer(raw_img)
-        return raw_img, mask_tensor
+        return raw_img, mask_tensor_long
 
     @staticmethod
     def get_mask(raw_pil, clean_pil):
@@ -127,63 +70,11 @@ class MaskLoader(DataLoader):
         clean_tensor = to_tensor(clean_pil)
         mask = torch.abs(raw_tensor - clean_tensor)  # usually use white to cover words
         mask = mask[0, :, :]  # single channel
-        mask = torch.where(mask > brightness_filter,  # filter difference in brightness
+        mask = torch.where(mask > brightness_difference,  # filter difference in brightness
                            torch.ones_like(mask),
                            torch.zeros_like(mask))
         return mask.long()
 
-
-class InpaintingData(MaskLoader):
-    def __init__(self, dataset,
-                 batch_size=2,
-                 shuffle=True):
-        super(MaskLoader, self).__init__(dataset,
-                                         batch_size,
-                                         shuffle,
-                                         )
-
-        self.dataset = dataset
-
-    @staticmethod
-    def add_random_mask(mask, length=5000):
-        # random walk
-        action_list = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        b, c, h, w = mask.size()
-        x = random.randint(h // 3, 4 * h // 5)
-        y = random.randint(w // 3, 4 * w // 5)
-        # x, y = random.randint(0, min(h, w)), random.randint(0, min(h, w))
-        for i in range(length):
-            step = random.choice(action_list)
-            x = min(h - 1, x + step[0])
-            y = min(w - 1, y + step[1])
-            mask[:, :, x, y] = 1
-        return mask
-
-    def batch_collector(self, batch):
-        raw_clean = self.process_images(batch)
-        raw_imgs = [self.img_transform(i[0]) for i in raw_clean]
-        raw_imgs = torch.stack(raw_imgs, dim=0).contiguous()
-
-        clean_imgs = [self.img_transform(i[1]) for i in raw_clean]
-        clean_imgs = torch.stack(clean_imgs, dim=0).contiguous()
-
-        # usually use white to cover words, so words' pixels become 1 after subtraction, the rest are 0
-        clean_img_masks = torch.abs(clean_imgs - raw_imgs)
-        clean_img_masks = self.add_random_mask(clean_img_masks)  # add 1s in random position
-
-        # masks are marked as 0, the rest are 1
-        clean_img_masks = torch.where(clean_img_masks > brightness_filter,  # filter difference in brightness
-                                      torch.zeros_like(clean_img_masks),
-                                      torch.ones_like(clean_img_masks))
-
-        clean_img_masks = clean_img_masks[:, :1, :, :].contiguous()
-        stacked_imgs = [torch.cat([i, j]) for i, j in zip(raw_imgs, clean_img_masks)]
-        stacked_imgs = torch.stack(stacked_imgs, dim=0)
-        if use_cuda:
-            stacked_imgs = stacked_imgs.cuda()
-            clean_imgs = clean_imgs.cuda(async=True)
-            # [img, mask],  mask,
-        return stacked_imgs, stacked_imgs[:, 3:4, :, :], clean_imgs
 
 
 class EvaluateSet(Dataset):
@@ -239,9 +130,3 @@ class EvaluateSet(Dataset):
             transforms.Lambda(lambda x: x.expand(-1, 3, -1, -1) > 0)
         ])
         return m
-
-#
-# transforms.Lambda(lambda x: x.squeeze(1).float()),
-# ToPILImage(),
-# Resize(tuple(reversed(origin_size))),  # torchvision uses (h, w)
-# ToTensor(),
