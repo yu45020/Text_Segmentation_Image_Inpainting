0f755c36c319c41a7b4ae7709e952463ccf543f1
yu45020 <yu45020@gmail.com>
2018-08-17 09:04:19 -0700

    test increase/decrease atrous
    Former-commit-id: 1b6964578dacf49d231c56df378d88dd8d27ec06
    Former-commit-id: 1583462b9bb4d6e3cac7dbaac50d7f1cf3b5ac7b

--- models/Xception.py
+++ models/Xception.py
@@ -51,8 +51,8 @@ class Xception(BaseModule):
         self.act_fn = act_fn
         self.entry_flow_1 = self.make_entry_flow_1(color_channel, 128)  # 1/4
         self.entry_flow_2 = self.make_entry_flow_2(128, 512)  # 1/16
-        self.middle_flow = self.make_middle_flow(512, 512, repeat_blocks=8, rate=2)
-        self.exit_flow = self.make_exit_flow(512, 512, rate=4)
+        self.middle_flow = self.make_middle_flow(512, 512, repeat_blocks=8, rate=(2, 4))
+        self.exit_flow = self.make_exit_flow(512, 512, rate=(2, 1))
         self.x4_feature_channels = 128
         self.last_feature_channels = 512
 
@@ -76,21 +76,33 @@ class Xception(BaseModule):
         )
         return m
 
-    def make_middle_flow(self, in_channel=728, out_channel=728, repeat_blocks=16, rate=1):
+    def make_middle_flow(self, in_channel=728, out_channel=728, repeat_blocks=16, rate=(2, 4)):
+
         m = []
-        for i in range(repeat_blocks):
-            m.append(ResidualBlock(in_channel, out_channel, 3, stride=1, padding=rate,
-                                   dilation=rate, bias=False, BN=True, activation=self.act_fn))
+        # for i in range(repeat_blocks):
+        #     m.append(ResidualBlock(in_channel, out_channel, 3, stride=1, padding=rate,
+        #                            dilation=rate, bias=False, BN=True, activation=self.act_fn))
+
+        #  Effective Use of Dilated Convolutions for Segmenting Small Object Instances in Remote Sensing Imagery
+        # by Ryuhei Hamaguchi & Aito Fujita & Keisuke Nemoto & Tomoyuki Imaizumi & Shuhei Hikosaka
+        for i in range(repeat_blocks // 2):
+            m.append(ResidualBlock(in_channel, out_channel, 3, stride=1, padding=rate[0],
+                                   dilation=rate[0], bias=False, BN=True, activation=self.act_fn))
+        for i in range(repeat_blocks // 2):
+            m.append(ResidualBlock(in_channel, out_channel, 3, stride=1, padding=rate[1],
+                                   dilation=rate[1], bias=False, BN=True, activation=self.act_fn))
         return nn.Sequential(*m)
 
-    def make_exit_flow(self, in_channel=728, out_channel=2048, rate=2):
+    def make_exit_flow(self, in_channel=728, out_channel=2048, rate=(2, 1)):
         m = nn.Sequential(
-            ResidualBlock(in_channel, 512, 3, stride=1, padding=rate, dilation=rate, bias=False,
+            ResidualBlock(in_channel, 512, 3, stride=1, padding=rate[0], dilation=rate[0], bias=False,
                           BN=True, activation=self.act_fn, expand_channel_first=False),
-            ResidualBlock(512, out_channel, 3, stride=1, padding=rate, dilation=rate, bias=False,
+            ResidualBlock(512, out_channel, 3, stride=1, padding=rate[0], dilation=rate[0], bias=False,
                           BN=True, activation=self.act_fn, expand_channel_first=False),
-            # DSConvBlock(512, out_channel, 3, 1, padding=rate, dilation=rate, bias=False,
-            #             BN=True, activation=self.act_fn)
+            ResidualBlock(512, out_channel, 3, stride=1, padding=rate[1], dilation=rate[1], bias=False,
+                          BN=True, activation=self.act_fn, expand_channel_first=False),
+            ResidualBlock(512, out_channel, 3, stride=1, padding=rate[1], dilation=rate[1], bias=False,
+                          BN=True, activation=self.act_fn, expand_channel_first=False),
         )
         return m
 
